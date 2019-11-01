4c3e35ff188c5e7b9e370fc93cea1b7012e350da
StefanieGlarcher <st.glarcher@gmail.com>
2019-11-01 08:54:10 +0100

    Addition, Subtraktion, Multiplikation und Division

---
+++ src/main/java/ch/zli/m223/punchclock/domain/Subtraktionen.java
@@ -0,0 +1,63 @@
+package ch.zli.m223.punchclock.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Subtraktionen {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private long zahlEins;
+
+    @Column(nullable = false)
+    private long zahlZwei;
+
+    @Column(nullable = false)
+    private long resultat;
+
+
+
+    public Subtraktionen(long id, long zahlEins, long zahlZwei, long resultat) {
+        this.id = id;
+        this.zahlEins = zahlEins;
+        this.zahlZwei = zahlZwei;
+        this.resultat = zahlEins - zahlZwei;
+    }
+
+    public Subtraktionen(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public long getZahlEins() {
+        return zahlEins;
+    }
+
+    public void setZahlEins(long zahlEins) {
+        this.zahlEins = zahlEins;
+    }
+
+    public long getZahlZwei() {
+        return zahlZwei;
+    }
+
+    public void setZahlZwei(long zahlZwei) {
+        this.zahlZwei = zahlZwei;
+    }
+
+    public long getResultat() {
+        return resultat;
+    }
+
+    public void setResultat(long resultat) {
+        this.resultat = resultat;
+    }
+}
